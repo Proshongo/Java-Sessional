9f8d2a458248c804feaa549bbb3cc8ef2374632d
Proshongo <proshongop@gmail.com>
2025-06-29 12:49:00 +0600

    TaskOne.java

---
+++ LabOne/TaskOne.java
@@ -0,0 +1,11 @@
+class Main
+{
+public static void main(String[] args) {
+String name = "Proshongo paul";
+String studentId = "E243039";
+String email = "proshongop@gmail.com";
+String number = "01732049896";
+String address = "Mirsarai";
+System.out.println("Name: " + name +"\nStudent Id: "+ studentId + "\nEmail: "+email +"\nPhone: "+ number +"\nAddress: "+ address);
+}
+}
